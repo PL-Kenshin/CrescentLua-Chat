9b626a4e46eace913174bce392e8b6934766d377
Polibus <33179@student.pwsztar.edu.pl>
2023-05-07 16:26:17 +0200

    splashScreenAndIconApp

--- android/app/src/main/java/com/projekt/MainActivity.java
+++ android/app/src/main/java/com/projekt/MainActivity.java
@@ -1,4 +1,7 @@
 package com.projekt;
+import android.os.Bundle; 
+
+import org.devio.rn.splashscreen.SplashScreen;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -6,6 +9,12 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
